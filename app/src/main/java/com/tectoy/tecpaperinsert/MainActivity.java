459b7a39d683ef2f2001070ea1f5aca6a1455747
Felipe do Nascimento <53831160+nascimentofe@users.noreply.github.com>
2021-03-31 17:27:02 -0300

    status bar e nav bar ajustados.

--- app/src/main/java/com/tectoy/tecpaperinsert/MainActivity.java
+++ app/src/main/java/com/tectoy/tecpaperinsert/MainActivity.java
@@ -3,6 +3,8 @@ package com.tectoy.tecpaperinsert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
     }
+
+    private void initViews() {
+    }
+
+    @Override
+    protected void onStart() {
+        hideStatusBarAndNavigationBar();
+        super.onStart();
+    }
+
+    private void hideStatusBarAndNavigationBar() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
 }
